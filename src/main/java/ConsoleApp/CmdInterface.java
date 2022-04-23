29ec26adda379dcab316857d88147918b61e48de
Charawey-X <charawey@gmail.com>
2022-04-23 22:44:14 +0300

    added scanner

--- src/main/java/ConsoleApp/CmdInterface.java
+++ src/main/java/ConsoleApp/CmdInterface.java
@@ -1,10 +1,18 @@
 package ConsoleApp;
 
+import java.util.Scanner;
+
 import static ConsoleApp.Cipher.encrypt;
 import static ConsoleApp.Decipher.decrypt;
 
 public class CmdInterface {
+    public static Scanner scanner =new Scanner(System.in);
     public static void main(String[] args) {
+        /**
+         * Test:
+         *
+         *
+         */
         String text = "Good boy";
         String ciphered = encrypt(text, 5);
         String deciphered = decrypt(ciphered, 5);
