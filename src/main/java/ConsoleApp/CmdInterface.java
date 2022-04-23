12001634abe8475b7f2d200ddb9c6f39b15d5e7b
Charawey-X <charawey@gmail.com>
2022-04-23 22:55:30 +0300

    comment-out test section

--- src/main/java/ConsoleApp/CmdInterface.java
+++ src/main/java/ConsoleApp/CmdInterface.java
@@ -8,15 +8,15 @@ import static ConsoleApp.Decipher.decrypt;
 public class CmdInterface {
     public static Scanner scanner =new Scanner(System.in);
     public static void main(String[] args) {
-        /**
+        /*
          * Test:
-         *
+         *String text = "Good boy";
+         *String ciphered = encrypt(text, 5);
+         *String deciphered = decrypt(ciphered, 5);
+         *System.out.println("Encrypted message: "+ciphered);
+         *System.out.println("Decrypted message: "+deciphered);
          *
          */
-        String text = "Good boy";
-        String ciphered = encrypt(text, 5);
-        String deciphered = decrypt(ciphered, 5);
-        System.out.println("Encrypted message: "+ciphered);
-        System.out.println("Decrypted message: "+deciphered);
+
     }
 }
