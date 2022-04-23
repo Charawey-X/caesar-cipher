18c25d9d010f7a1c4ebfff5e19f40f44bbbdf58d
Charawey-X <charawey@gmail.com>
2022-04-23 22:22:21 +0300

    added test text

--- src/main/java/ConsoleApp/CmdInterface.java
+++ src/main/java/ConsoleApp/CmdInterface.java
@@ -1,4 +1,11 @@
 package ConsoleApp;
 
+import static ConsoleApp.Cipher.encrypt;
+
 public class CmdInterface {
+    public static void main(String[] args) {
+        String text = "Good boy";
+        String ciphered = encrypt(text, 5);
+        System.out.println(ciphered);
+    }
 }
