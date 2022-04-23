0b8ec8072bdd208ef928a33868003c462fab0039
Charawey-X <charawey@gmail.com>
2022-04-23 22:42:00 +0300

    test run

--- src/main/java/ConsoleApp/CmdInterface.java
+++ src/main/java/ConsoleApp/CmdInterface.java
@@ -5,8 +5,10 @@ import static ConsoleApp.Decipher.decrypt;
 
 public class CmdInterface {
     public static void main(String[] args) {
-        String text = "Ltti gtd";
-        String ciphered = decrypt(text, 5);
-        System.out.println(ciphered);
+        String text = "Good boy";
+        String ciphered = encrypt(text, 5);
+        String deciphered = decrypt(ciphered, 5);
+        System.out.println("Encrypted message: "+ciphered);
+        System.out.println("Decrypted message: "+deciphered);
     }
 }
