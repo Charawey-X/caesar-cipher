4cf483d8767f175150539e5b80a71e8930de06ac
Charawey-X <charawey@gmail.com>
2022-04-23 21:39:24 +0300

    added shift condition

--- src/main/java/ConsoleApp/Cipher.java
+++ src/main/java/ConsoleApp/Cipher.java
@@ -1,4 +1,17 @@
 package ConsoleApp;
 
 public class Cipher {
+    public static String encrypt(String message, int shift){
+        if(shift>26){
+            shift = shift % 26;
+        }
+        else if (shift<0) {
+            shift =(shift%26)+26;
+            
+        }
+
+        String cipherText ="";
+        int length = message.length();
+        return cipherText;
+    }
 }
